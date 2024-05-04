fead1d82458c4d2cf79c5f30f20dc958c674ec28
Navodita Mathur <navoditamathur1998@gmail.com>
2024-05-04 17:06:11 -0400

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from .TrectextCollection import TrectextCollection
+from .TrecwebCollection import TrecwebCollection
+from .WordNormalizer import WordNormalizer
+from .WordTokenizer import WordTokenizer
+from .StopWordRemover import StopWordRemover
